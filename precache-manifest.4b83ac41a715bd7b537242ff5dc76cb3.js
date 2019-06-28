4fdb0bdef493c5f7681b56d67023edede79fb578
mikemikemikemikemmmm <36297097+mikemikemikemikemmmm@users.noreply.github.com>
2019-06-28 16:20:37 +0800

    6/28

---
+++ precache-manifest.4b83ac41a715bd7b537242ff5dc76cb3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7efcce227a836f81a30f852250f8467",
+    "url": "/five/index.html"
+  },
+  {
+    "revision": "7aa0da1bc3467b1a91df",
+    "url": "/five/static/css/main.84c5d2ad.chunk.css"
+  },
+  {
+    "revision": "49f9a9fda4fce6e86635",
+    "url": "/five/static/js/2.d4bb07b6.chunk.js"
+  },
+  {
+    "revision": "7aa0da1bc3467b1a91df",
+    "url": "/five/static/js/main.46af5ec9.chunk.js"
+  },
+  {
+    "revision": "df15e1a0728d950e5699",
+    "url": "/five/static/js/runtime~main.41cc362f.js"
+  }
+]);
